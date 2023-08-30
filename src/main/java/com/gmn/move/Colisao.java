bd3e1d0e89afc17213c46125b7f7731d268479ea
gustavoma <gustavo.macedo@duxus.com.br>
2023-08-30 00:49:57 -0300

    add Colisao

---
+++ src/main/java/com/gmn/move/Colisao.java
@@ -0,0 +1,50 @@
+package com.gmn.move;
+
+import com.gmn.view.Comida;
+import com.gmn.view.Snake;
+
+import java.awt.*;
+
+public class Colisao {
+
+    private final Snake snake;
+    private int widthTela;
+    private int heightTela;
+
+    public Colisao(Snake snake, int widthTela, int heightTela) {
+        this.snake = snake;
+        this.widthTela = widthTela;
+        this.heightTela = heightTela;
+    }
+
+    /**
+     * Verifica a distancia da cobra e da comida para verificar se no próximo frame havera colisão
+     * */
+    public boolean colisaoComComida(Point novaPosicaoCabeca, Comida comida){
+        int distanciaX = Math.abs(novaPosicaoCabeca.x - comida.getPontoComida().x);
+        int distanciaY = Math.abs(novaPosicaoCabeca.y - comida.getPontoComida().y);
+        return distanciaX <= snake.getWidth() && distanciaY <= snake.getHeight();
+    }
+
+    public boolean colisaoComParedes(Point novaPosicaoCabeca){
+        if(novaPosicaoCabeca.x >= widthTela - 2
+                || novaPosicaoCabeca.x <= 0
+                || novaPosicaoCabeca.y >= heightTela - 30
+                || novaPosicaoCabeca.y <= 0){
+            System.out.println("BATEU");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean colisaoComProprioCorpo(Point novaPosicaoCabeca){
+        return snake.getCorpo().stream().anyMatch(
+                corpoSnake -> {
+                    int distanciaX = Math.abs(corpoSnake.x - novaPosicaoCabeca.x);
+                    int distanciaY = Math.abs(corpoSnake.y - novaPosicaoCabeca.y);
+                    return distanciaX <= snake.getWidth() && distanciaY <= snake.getHeight();
+                }
+        );
+    }
+
+}
