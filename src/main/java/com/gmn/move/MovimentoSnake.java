1840f222599706c1eee6128741d3f6accc31eb9d
gustavoma <gustavo.macedo@duxus.com.br>
2023-09-07 16:33:09 -0300

    atualizando score quando cobra se alimenta

--- src/main/java/com/gmn/move/MovimentoSnake.java
+++ src/main/java/com/gmn/move/MovimentoSnake.java
@@ -2,6 +2,7 @@ package com.gmn.move;
 
 import com.gmn.view.Comida;
 import com.gmn.view.Snake;
+import com.gmn.view.TelaFrame;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -9,19 +10,22 @@ import java.awt.event.KeyListener;
 
 public class MovimentoSnake implements KeyListener {
 
-    private Snake snake;
-    private Comida comida;
+    private final Snake snake;
+    private final Comida comida;
     private KeyEvent novaDirecao;
     private int direcaoAtual;
     private final Colisao colisao;
     private final int widthTela;
     private final int heightTela;
 
-    public MovimentoSnake(Snake snake, Comida comida, int widthTela, int heightTela) {
+    private final TelaFrame telaFrame;
+
+    public MovimentoSnake(Snake snake, Comida comida, int widthTela, int heightTela, TelaFrame telaFrame) {
         this.snake = snake;
         this.comida = comida;
         this.widthTela = widthTela;
         this.heightTela = heightTela;
+        this.telaFrame = telaFrame;
         this.colisao = new Colisao(snake, this.widthTela, this.heightTela);
     }
 
@@ -59,6 +63,7 @@ public class MovimentoSnake implements KeyListener {
                 if (colisao.colisaoComComida(novaPosicaoCabeca, comida)) {
                     snake.getCorpo().addFirst(novaPosicaoCabeca);
                     comida.posicionarComida();
+                    telaFrame.updateTitle(1);
                 } else {
                     snake.getCorpo().addFirst(novaPosicaoCabeca);
                     snake.getCorpo().removeLast();
