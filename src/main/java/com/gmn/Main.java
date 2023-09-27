49c2ace74fb1c015d265c849c33efeb986757bf7
gustavoma <gustavo.macedo@duxus.com.br>
2023-09-27 00:06:13 -0300

    registrando e mostrando score

--- src/main/java/com/gmn/Main.java
+++ src/main/java/com/gmn/Main.java
@@ -1,11 +1,13 @@
 package com.gmn;
 
 import com.gmn.view.TelaFrame;
+import com.gmn.view.score.RegistraPontuacaoGeral;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        RegistraPontuacaoGeral.criaCaminhoSave();
         SwingUtilities.invokeLater(TelaFrame::new);
     }
 }
